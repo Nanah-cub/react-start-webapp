f013e415a47f3c7c07ef7415851fd301090bb65f
Josh Buchanan <JoshBuchanan@Joshs-MacBook-Pro.local>
2017-12-20 20:38:42 -0500

    Preeject

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import './pollyfills';
+import './vendor';
+import AppComponent from './app/app';
 
 
+ReactDOM.render(
+    <AppComponent />,
+    document.getElementById('root')
+);
--- src/pollyfills.js
+++ src/pollyfills.js
@@ -20,24 +20,5 @@
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-
-
-
-
-
-
-
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
-/**
- * Date, currency, decimal and percent pipes.
- * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
 import 'intl';
-/**
- * Need to import at least one locale-data with intl.
- */
 import 'intl/locale-data/jsonp/en';
