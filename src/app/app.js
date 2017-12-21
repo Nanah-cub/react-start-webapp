3496adaad576f97195b6fb66ad9dd23c3b1adfa3
Josh Buchanan <JoshBuchanan@Joshs-MacBook-Pro.local>
2017-12-20 20:39:24 -0500

    Adding untracked files

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class AppComponent extends React.Component {
+    render() {
+        return (
+            <div>Josh</div>
+        );
+    }
+}
+
+export default AppComponent;
